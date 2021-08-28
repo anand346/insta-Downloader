86a33dcfc70f86c1a1d5bbbfb1c076104caa472b
anand346 <rajanand9039@gmail.com>
2021-08-28 09:04:07 +0530

    php index page added

---
+++ indexPage.php
@@ -0,0 +1,44 @@
+<?php include "includes/header.php"; ?>
+<section id="hero">
+        <div class="head">
+            <div class="heading">
+                <h1>INSTA DOWNLOADER</h1>
+            </div>
+            <div class="sub-heading">
+                <h3>Download Instagram stuff with just one click</h3>
+            </div>
+        </div>
+        <div class="body">
+            <div class="body-image">
+                <img src="assets/images/insta.png" alt="">
+            </div>
+            <div class="body-desc">
+                <h2>HOW IT WORKS ?</h2>
+                <div class="circles">
+                    <div class="circle-1 circle">1</div>
+                    <div class="joiner"></div>
+                    <div class="circle-2 circle">2</div>
+                    <div class="joiner"></div>
+                    <div class="circle-3 circle">3</div>
+                </div>
+                <div class="steps">
+                    <div class="step-1 step">
+                        <h4>Step #1</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe maiores fugiat, neque id dignissimos incidunt libero excepturi ducimus vero itaque.</p>
+                    </div>
+                    <div class="step-2 step">
+                        <h4>Step #2</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe maiores fugiat, neque id dignissimos incidunt libero excepturi ducimus vero itaque.</p>
+                    </div>
+                    <div class="step-3 step">
+                        <h4>Step #3</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe maiores fugiat, neque id dignissimos incidunt libero excepturi ducimus vero itaque.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php include "includes/features.php"; ?>
+<?php include "includes/team.php"; ?>
+<?php include "includes/contact.php"; ?>
+<?php include "includes/footer.php"; ?>
