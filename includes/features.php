3cb8b71f639d2b79a271c6a9a8afb1aa4b6056a4
anand346 <rajanand9039@gmail.com>
2021-08-29 17:52:07 +0530

    added link on each anchor tag

--- includes/features.php
+++ includes/features.php
@@ -9,101 +9,113 @@
         </div>
         <div class="body">
             <div class="left-features" data-animate="bounceIn" data-offset="50" data-duration="1.0s" data-delay="0.1s">
-                <div class="download-profile f-l">
-                    <div class="content">
-                        <div class="head">
-                            <h4>DOWNLOAD PROFILE #1</h4>
+                <a href="downloadProfile.php">
+                    <div class="download-profile f-l">
+                        <div class="content">
+                            <div class="head">
+                                <h4>DOWNLOAD PROFILE #1</h4>
+                            </div>
+                            <div class="body">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, tempore.</p>
+                            </div>
                         </div>
-                        <div class="body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, tempore.</p>
-                        </div>
-                    </div>
-                    <div class="image">
-                        <div class="image-a">
-                            <img src="assets/images/account.png" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="extract-tags f-l">
-                    <div class="content">
-                        <div class="head">
-                            <h4>EXTRACT TAGS #3</h4>
-                        </div>
-                        <div class="body">
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, at?</p>
+                        <div class="image">
+                            <div class="image-a">
+                                <img src="assets/images/account.png" alt="">
+                            </div>
                         </div>
                     </div>
-                    <div class="image">
-                        <div class="image-a">
-                            <img src="assets/images/tags.png" alt="">
+                </a>
+                <a href="extractTags.php">
+                    <div class="extract-tags f-l">
+                        <div class="content">
+                            <div class="head">
+                                <h4>EXTRACT TAGS #3</h4>
+                            </div>
+                            <div class="body">
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, at?</p>
+                            </div>
+                        </div>
+                        <div class="image">
+                            <div class="image-a">
+                                <img src="assets/images/tags.png" alt="">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="download-photos f-l">
-                    <div class="content">
-                        <div class="head">
-                            <h4>DOWNLOAD PHOTOS #5</h4>
+                </a>
+                <a href="downloadPhoto.php">
+                    <div class="download-photos f-l">
+                        <div class="content">
+                            <div class="head">
+                                <h4>DOWNLOAD PHOTOS #5</h4>
+                            </div>
+                            <div class="body">
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptatum?</p>
+                            </div>
                         </div>
-                        <div class="body">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, voluptatum?</p>
+                        <div class="image">
+                            <div class="image-a">
+                                <img src="assets/images/photo.png" alt="">
+                            </div>
                         </div>
                     </div>
-                    <div class="image">
-                        <div class="image-a">
-                            <img src="assets/images/photo.png" alt="">
-                        </div>
-                    </div>
-                </div>
+                </a>
             </div>
             <div class="center-image" data-animate="bounceIn" data-offset="50" data-duration="1.0s" data-delay="0.1s">
                 <img src="assets/images/features.jpg" alt="">
             </div>
             <div class="right-features" data-animate="bounceIn" data-offset="50" data-duration="1.0s" data-delay="0.1s">
-                <div class="download-reels f-r">
-                    <div class="image">
-                        <div class="image-a">
-                            <img src="assets/images/instagram-reels.png" alt="">
+                <a href="downloadReel.php">    
+                    <div class="download-reels f-r">
+                        <div class="image">
+                            <div class="image-a">
+                                <img src="assets/images/instagram-reels.png" alt="">
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="head">
+                                <h4>DOWNLOAD REELS #2</h4>
+                            </div>
+                            <div class="body">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, praesentium?</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="content">
-                        <div class="head">
-                            <h4>DOWNLOAD REELS #2</h4>
+                </a>
+                <a href="downloadVideo.php">
+                    <div class="download-videos f-r">
+                        <div class="image">
+                            <div class="image-a">
+                                <img src="assets/images/video.png" alt="">
+                            </div>
                         </div>
-                        <div class="body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, praesentium?</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="download-videos f-r">
-                    <div class="image">
-                        <div class="image-a">
-                            <img src="assets/images/video.png" alt="">
-                        </div>
-                    </div>
-                    <div class="content">
-                        <div class="head">
-                            <h4>DOWNLOAD VIDEOS #4</h4>
-                        </div>
-                        <div class="body">
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, adipisci?</p>
+                        <div class="content">
+                            <div class="head">
+                                <h4>DOWNLOAD VIDEOS #4</h4>
+                            </div>
+                            <div class="body">
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, adipisci?</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="download-igtv f-r">
-                    <div class="image">
-                        <div class="image-a">
-                            <img src="assets/images/igtv.png" alt="">
+                </a>
+                <a href="downloadIgtv.php">
+                    <div class="download-igtv f-r">
+                        <div class="image">
+                            <div class="image-a">
+                                <img src="assets/images/igtv.png" alt="">
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="head">
+                                <h4>DOWNLOAD IGTV #6</h4>
+                            </div>
+                            <div class="body">
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, minus.</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="content">
-                        <div class="head">
-                            <h4>DOWNLOAD IGTV #6</h4>
-                        </div>
-                        <div class="body">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, minus.</p>
-                        </div>
-                    </div>
-                </div>
+                </a>
             </div>
         </div>
     </section>
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
+<meta name="Description" content="Instagram downloader used for downloading instagram reels,videos,photos,IGTV,profile picture and extracting tags"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="assets/css/style.css">
@@ -20,7 +20,7 @@
     </div>
     <header id = "header">
         <div class="logo">
-            <a href="#home"><span>IG</span>Download</a>
+            <a href="indexPage.php"><span>IG</span>Download</a>
         </div>
         <div class="navigation">
             <ul>
