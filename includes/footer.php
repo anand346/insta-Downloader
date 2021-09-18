237a5e91529d73761536e158c434d471f1be133c
anand346 <rajanand9039@gmail.com>
2021-09-18 14:50:04 +0530

    18-09-2021 14:49 added warning to DOM

--- includes/footer.php
+++ includes/footer.php
@@ -150,6 +150,7 @@
                 type : "POST",
                 data : {url : url, action : action, csrf_token : csrf_token },
                 success : function(data){
+                    console.log(data);
                     var all_url = JSON.parse(data);
                     $("section#downloadable .all_contents").html("");
                     if("mismatchCsrf" in all_url[0] || "hostError" in all_url[0] || "invalidAction" in all_url[0]){
